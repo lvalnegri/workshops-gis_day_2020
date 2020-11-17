60cda035d7f5d6422160dfb7a31e43f1c3e7420c
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-11-17 17:49:13 +0000

    renamed

--- 01-install_packages.R
+++ 01-install_packages.R
@@ -7,16 +7,16 @@ pkgs <- c(
     # data collection and wrangling 
     'data.table', 'fst', 'dplyr', 'openxlsx', 'readxl', 'tidyr',
     # data display
-    'DT', 'formattable', 'lineupjs', 'pivta', 'rhandsontable', 'rpivotTable', 
-    'flextable', 'htmlTable', 'knitr', 'kableExtra', 'huxtable', 'pixiedust', 'basictabler',
+    'DT', 'formattable', 'lineupjs', 'pivta', ' reactable', 'rhandsontable', 'rpivotTable', 
+     'basictabler', 'flextable', 'gt', 'htmlTable', 'knitr', 'kableExtra', 'huxtable', 'pixiedust',
     # data visualization
-    'ggplot2', 'ggiraph', 'ggthemes',
-    'htmlwidgets', 'leaflet', 'leaflet.extras', 'leaflet.extras2', 'leafsync', 'rbokeh',
+    'ggplot2', 'ggiraph', 'ggmap', 'ggthemes', 'htmlwidgets', 'rbokeh',
      'classInt', 'scales', 'paletteer',
     # data presentation / deployment
-    'htmltools', 'rmarkdown', 'shiny', 'shinyjs', 'shinyscreenshot', 'shinyWidgets',
+    'htmltools', 'rmarkdown', 'shiny', 'shinyjs', 'shinyscreenshot', 'shinyWidgets', 'xaringan',
     # spatial tools
-    'ggmap', 'ggspatial', 'maptools', 'mapview', 'raster', 'rgdal', 'rgeos', 'rmapshaper', 'sf', 'sp', 'tmap'
+    'ggmap', 'ggspatial', 'leaflet', 'leaflet.extras', 'leaflet.extras2', 'leafpop', 'leafsync', 
+    'maptools', 'mapview', 'raster', 'rgdal', 'rgeos', 'rmapshaper', 'sf', 'sp', 'tmap'
 )
 pkgs.not <- pkgs[!sapply(pkgs, require, char = TRUE)]
 if(length(pkgs.not) > 0) install.packages(pkgs.not)
